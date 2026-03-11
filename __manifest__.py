5b7fe33694943130a8416747f91268ade81c3a9a
pmorettobr <pmorettobr@gmail.com>
2026-03-11 12:19:38 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,19 @@
-
+{
+    'name': 'Cylinder Production Tracking',
+    'version': '16.0.1.0',
+    'summary': 'Rastreamento de produção de cilindros',
+    'author': 'Paulo Moretto',
+    'depends': [
+        'mrp',
+        'stock'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/tracking_views.xml',
+        'views/menu.xml',
+        'reports/report_production.xml',
+        'reports/report_production_template.xml'
+    ],
+    'installable': True,
+    'application': True
+}
